dfec409bb0469fa59ba16e1dff507cafe8d6eda3
andremoura98 <andremoura98@gmail.com>
2019-06-18 16:50:51 +0100

    Update credenciais.py

--- credenciais.py
+++ credenciais.py
@@ -1,4 +1,4 @@
-Consumer_key="rFsMOk1iT31wHf2WgJWQI1vMM"
-Consumer_secret_key="20RMu3dJcCqIeYvy1buB4SCTBSJfS0SoPq95ZCXOjAJG38oroG"
-Access_Token="972113781592612864-FpkGwKLeG1CNMwMEwNlouPkU8Fik4Zi"
-Access_Secret_Token="vQWtdk1s2SgirlQGXaUSMSnxdCvz9kIXlYSuE1H7Ndt82"
+Consumer_key="enter your key"
+Consumer_secret_key="enter your key"
+Access_Token="enter your key"
+Access_Secret_Token="enter your key"
